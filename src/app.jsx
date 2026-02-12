6d7189ee4199fd2dc68baed4a084ab75371997e2
Crystal Hwang <clhwang@byu.edu>
2026-02-12 14:39:01 -0700

    creating the router

--- src/app.jsx
+++ src/app.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Photo } from './photo/photo';
+import { Recipe } from './recipe/recipe';
+import { Login } from './login/login';
 
 export default function App() {
   return (
-    <div>
-        <header>
-        <h1>Food Fetch</h1>
-        <nav>
-            <menu>
-            <li><a href="index.html" className='main-color'>Home</a></li>
-            <li><a href="recipe.html" className='main-color'>Recipes</a></li>
-            <li><a href="photo.html" className='main-color'>Photo Gallery</a></li>
-            </menu>
-        </nav>
-        </header>
+    <BrowserRouter>
+      <div>
+          <header>
+          <h1>Food Fetch</h1>
+          <nav>
+              <menu>
+              <li><a href="index.html" className='main-color'>Home</a></li>
+              <li><a href="recipe.html" className='main-color'>Recipes</a></li>
+              <li><a href="photo.html" className='main-color'>Photo Gallery</a></li>
+              </menu>
+          </nav>
+          </header>
 
-        <main>App components go here</main>
+          <main>App components go here</main>
 
-        <footer>
-        <hr />
-        <span className="text-reset">Crystal Hwang </span>
-        <br />
-        <a href="https://github.com/crystalhwaang/startup">GitHub</a>
-        </footer>
-    </div>
+          <footer>
+          <hr />
+          <span className="text-reset">Crystal Hwang </span>
+          <br />
+          <a href="https://github.com/crystalhwaang/startup">GitHub</a>
+          </footer>
+      </div>
+    </BrowserRouter>
   );
 }
