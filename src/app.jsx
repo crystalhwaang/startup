76c3397be5287eea5c53a6460fedd58da920fee7
Crystal Hwang <clhwang@byu.edu>
2026-02-12 00:37:52 -0700

    move all manual css to app.css and created app component

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div>
+        <header>
+        <h1>Food Fetch</h1>
+        <nav>
+            <menu>
+            <li><a href="index.html" className='main-color'>Home</a></li>
+            <li><a href="recipe.html" className='main-color'>Recipes</a></li>
+            <li><a href="photo.html" className='main-color'>Photo Gallery</a></li>
+            </menu>
+        </nav>
+        </header>
+
+        <main>App components go here</main>
+
+        <footer>
+        <hr />
+        <span className="text-reset">Crystal Hwang </span>
+        <br />
+        <a href="https://github.com/crystalhwaang/startup">GitHub</a>
+        </footer>
+    </div>
+  );
 }
