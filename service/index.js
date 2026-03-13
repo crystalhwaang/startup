418d1240313adf2d9d3827e7f8ca18fcdd49622b
Crystal Hwang <clhwang@byu.edu>
2026-03-12 18:48:58 -0600

    secure endpoints

--- service/index.js
+++ service/index.js
@@ -1,12 +1,126 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+
+const authCookieName = "token";
 const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
-app.get(/.*/, (_req, res) => {
-  res.send({ msg: 'Simon service' });
-});
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+app.use(express.json());
+app.use(cookieParser());
+
+let users = [];
+let photos = [];
+
+let apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
+});
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+};
+
+// GetPhotos
+apiRouter.get('/photos', verifyAuth, (_req, res) => {
+  res.send(photos);
+});
+
+// SubmitPhotos
+apiRouter.post('/photo', verifyAuth, (req, res) => {
+  photos = updatePhotos(req.body);
+  res.send(photos);
+});
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+// updatePhotos considers a new photo in the feed
+function updatePhotos(newPhoto) {
+  photos.unshift(newPhoto);
+  if (photos.length > 20) {
+    photos.length = 20;
+  }
+  return photos;
+}
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
