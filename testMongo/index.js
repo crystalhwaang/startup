796ef969d0c4784d1ea70fa046b7ec5c0a80d7d1
Crystal Hwang <clhwang@byu.edu>
2026-03-24 21:35:49 -0600

    connect backend

--- testMongo/index.js
+++ testMongo/index.js
@@ -1,50 +1,56 @@
-const { MongoClient } = require('mongodb');
-const config = require('./dbConfig.json');
+// const { MongoClient } = require('mongodb');
+// const config = require('./dbConfig.json');
 
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+// const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
-// Connect to the database cluster
-const client = new MongoClient(url);
-const db = client.db('rental');
-const collection = db.collection('house');
+// // Connect to the database cluster
+// const client = new MongoClient(url);
+// const db = client.db('rental');
+// const collection = db.collection('house');
 
-async function main() {
-  try {
-    // Test that you can connect to the database
-    await db.command({ ping: 1 });
-    console.log(`DB connected to ${config.hostname}`);
-  } catch (ex) {
-    console.log(`Connection failed to ${url} because ${ex.message}`);
-    process.exit(1);
-  }
+// async function main() {
+//   try {
+//     // Test that you can connect to the database
+//     await db.command({ ping: 1 });
+//     console.log(`DB connected to ${config.hostname}`);
+//   } catch (ex) {
+//     console.log(`Connection failed to ${url} because ${ex.message}`);
+//     process.exit(1);
+//   }
 
-  try {
-    // Insert a document
-    const house = {
-      name: 'Beachfront views',
-      summary: 'From your bedroom to the beach, no shoes required',
-      property_type: 'Condo',
-      beds: 1,
-    };
-    await collection.insertOne(house);
+//   try {
+//     // Insert a document
+//     const house = {
+//       name: 'Beachfront views',
+//       summary: 'From your bedroom to the beach, no shoes required',
+//       property_type: 'Condo',
+//       beds: 1,
+//     };
+//     await collection.insertOne(house);
 
-    // Query the documents
-    const query = { property_type: 'Condo', beds: { $lt: 2 } };
-    const options = {
-      sort: { name: -1 },
-      limit: 10,
-    };
-    const cursor = collection.find(query, options);
-    const rentals = await cursor.toArray();
-    rentals.forEach((i) => console.log(i));
+//     // Query the documents
+//     const query = { property_type: 'Condo', beds: { $lt: 2 } };
+//     const options = {
+//       sort: { name: -1 },
+//       limit: 10,
+//     };
+//     const cursor = collection.find(query, options);
+//     const rentals = await cursor.toArray();
+//     rentals.forEach((i) => console.log(i));
 
-    // Delete documents
-    await collection.deleteMany(query);
-  } catch (ex) {
-    console.log(`Database (${url}) error: ${ex.message}`);
-  } finally {
-    await client.close();
-  }
-}
+//     // Delete documents
+//     await collection.deleteMany(query);
+//   } catch (ex) {
+//     console.log(`Database (${url}) error: ${ex.message}`);
+//   } finally {
+//     await client.close();
+//   }
+// }
 
-main();
+// main();
+
+const DB = require('database.js');
+
+let users = [];
+let recipes = [];
+let photos = [];
