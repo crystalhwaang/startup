06520199e72384f9494c59049f8c629feb6e27c6
Crystal Hwang <clhwang@byu.edu>
2026-03-24 22:56:34 -0600

    updating index to database use

--- service/index.js
+++ service/index.js
@@ -41,14 +41,15 @@ apiRouter.post('/auth/create', async (req, res) => {
 // GetAuth login an existing user
 apiRouter.post('/auth/login', async (req, res) => {
   const user = await findUser('email', req.body.email);
-  if (user) {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      user.token = uuid.v4();
-      setAuthCookie(res, user.token);
-      res.send({ email: user.email });
-      return;
-    }
+  if (user && await bcrypt.compare(req.body.password, user.password)) {
+    user.token = uuid.v4();
+    await DB.updateUser(user);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+    return;
   }
+
   res.status(401).send({ msg: 'Unauthorized' });
 });
 
@@ -57,6 +58,7 @@ apiRouter.delete('/auth/logout', async (req, res) => {
   const user = await findUser('token', req.cookies[authCookieName]);
   if (user) {
     delete user.token;
+    await DB.updateUser(user);
   }
   res.clearCookie(authCookieName);
   res.status(204).end();
@@ -73,13 +75,20 @@ const verifyAuth = async (req, res, next) => {
 };
 
 // GetPhotos
-apiRouter.get('/photos', verifyAuth, (_req, res) => {
+apiRouter.get('/photos', verifyAuth, async (_req, res) => {
+  const photos = await DB.getPhotos();
   res.send(photos);
 });
 
 // SubmitPhotos
-apiRouter.post('/photo', verifyAuth, (req, res) => {
-  photos = updatePhotos(req.body);
+apiRouter.post('/photo', verifyAuth, async (req, res) => {
+  const photo = {
+    ...req.body,
+    createdAt: new Date(),
+  };
+
+  await DB.addPhoto(photo);
+  const photos = await DB.getPhotos();
   res.send(photos);
 });
 
@@ -94,13 +103,13 @@ app.use((_req, res) => {
 });
 
 // updatePhotos considers a new photo in the feed
-function updatePhotos(newPhoto) {
-  photos.unshift(newPhoto);
-  if (photos.length > 20) {
-    photos.length = 20;
-  }
-  return photos;
-}
+// function updatePhotos(newPhoto) {
+//   photos.unshift(newPhoto);
+//   if (photos.length > 20) {
+//     photos.length = 20;
+//   }
+//   return photos;
+// }
 
 async function createUser(email, password) {
   const passwordHash = await bcrypt.hash(password, 10);
@@ -118,7 +127,15 @@ async function createUser(email, password) {
 async function findUser(field, value) {
   if (!value) return null;
 
-  return users.find((u) => u[field] === value);
+  if (field === 'token') {
+    return await DB.getUserByToken(value);
+  }
+
+  if (field === 'email') {
+    return await DB.getUser(value);
+  }
+
+  return null;
 }
 
 // setAuthCookie in the HTTP response
